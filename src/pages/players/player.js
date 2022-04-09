432bd45e7d1e07e72ad93ee951d49e2d127c60f2
Xin Liu <xinliu.gl@gmail.com>
2022-04-08 23:13:28 -0700

    setup api adapter

--- src/pages/players/player.js
+++ src/pages/players/player.js
@@ -1,10 +1,29 @@
 import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { fetchData } from '../../utils/api/index';
 
 export default function PlayerPage() {
   let params = useParams();
-  return (
-    <main style={{ padding: '1rem' }}>
-      <h2>Player ID: {params.playerId}</h2>
-    </main>
-  );
+
+  const [player, setPlayer] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetchData(`/bbplayer/${params.playerId}/season/13`).then((data) => {
+      setPlayer(data);
+      setIsLoaded(true);
+    });
+  }, [params.playerId]);
+  console.log(player)
+  if (isLoaded){
+    return (
+      <main style={{ padding: '1rem' }}>
+        <h2>{player.PlayerName}</h2>
+      </main>
+    );
+  } else {
+    return 'LOADING'
+  }
+
+    
 }
