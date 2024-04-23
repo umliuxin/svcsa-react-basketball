7ed44ba6faecd0138c53350bca6818256326bfeb
vivianzhang <jzhang230@usfca.edu>
2024-04-23 12:43:01 -0700

    Fixed the team logo uploading issues

--- components/basketball/teams/TeamCard.tsx
+++ components/basketball/teams/TeamCard.tsx
@@ -1,29 +1,31 @@
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { useState } from "react";
 
-interface TeamCardProp{
-    team: any;
+interface TeamCardProp {
+  team: any;
 }
 
-const TeamCard: React.FC<TeamCardProp> = ( {team} )=> {
-    return (
-        <Card key={team.id} isPressable>
-            <CardBody className="justify-center items-center h-40 hover:opacity-50">
-              <Image
-                width={100}
-                height={100}
-                alt={team.name}
-                src={
-                  team.logosrc === "http://svcsa.org/uploads/null"
-                    ? "http://www.svcsa.org/images/teamlogo.jpg"
-                    : team.logosrc
-                }  
-              />
-            </CardBody>
-            <CardFooter className="h-15 bg-neutral-300 justify-center">
-              <p className="text-small">{team.shortname}</p>
-            </CardFooter>
-          </Card>
-    );
-}
+const TeamCard: React.FC<TeamCardProp> = ({ team }) => {
+  const [visibility, setVisibility] = useState("invisible");
+  return (
+    <Card key={team.id} className="relative" isPressable>
+      <CardBody className="justify-center items-center h-40 hover:opacity-50">
+        <Image
+          width={100}
+          height={100}
+          alt={team.name}
+          src={team.logosrc}
+          onError={() => {
+            setVisibility("visible");
+          }}
+        />
+        <p className={`${visibility} absolute font-medium text-6xl`}>{team.shortname.substring(0,1).toUpperCase()}</p>
+      </CardBody>
+      <CardFooter className="h-15 bg-neutral-300 justify-center">
+        <p className="text-small">{team.shortname}</p>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default TeamCard;
