bdcf961d128aaac20029832a21b67c890009a8a5
Xin Liu <xinliu.gl@gmail.com>
2024-07-19 19:52:41 -0700

    add header

--- utils/variables.ts
+++ utils/variables.ts
@@ -1,11 +1,19 @@
+export const MEN_OPEN = 'men-open';
+export const MEN_SENIOR = 'men-senior';
+export const WOMEN_OPEN = 'women-open';
+
+export const ALL_COMPETITION_GROUPS = [MEN_OPEN, MEN_SENIOR, WOMEN_OPEN];
+
 export const GROUPNAME_TO_COMPETITIONID: Record<string, number> = {
-  "men-open": 1,
-  "men-senior": 4,
-  "women-open": 2,
+  [MEN_OPEN]: 1,
+  [MEN_SENIOR]: 4,
+  [WOMEN_OPEN]: 2,
 };
 
 export const COMPETITIONID_TO_GROUPNAME: Record<number, string> = {
-  1: "men-open",
-  4: "men-senior",
-  2: "women-open",
+  1: MEN_SENIOR,
+  4: MEN_SENIOR,
+  2: WOMEN_OPEN,
 };
+
+
