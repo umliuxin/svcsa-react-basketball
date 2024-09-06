7b9f44f27c6fd357d11a4b697622466afc372e1c
Xin Liu <xinliu.gl@gmail.com>
2024-09-06 15:03:09 -0700

    fix the blocker

--- components/basketball/matchStat/MatchSumTable.tsx
+++ components/basketball/matchStat/MatchSumTable.tsx
@@ -1,68 +1,108 @@
 "use client";
 
-import { Table, TableBody, TableCell, TableHeader, TableRow } from '@nextui-org/react';
-import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+  TableColumn,
+} from "@nextui-org/react";
+import React from "react";
 
 interface MatchSumTableProps {
-    sections: number;
-    teamAname: string;
-    teamBname: string;
-    teamAscores: number[];
-    teamBscores: number[];
+  sections: number;
+  teamAname: string;
+  teamBname: string;
+  teamAscores: number[];
+  teamBscores: number[];
 }
 
-const MatchSumTable: React.FC<MatchSumTableProps> = ({sections,teamAname, teamBname, teamAscores, teamBscores}) => {
-    return (
-        <div className="p-5 bg-gray-50 rounded-lg overflow-x-auto">
-          <h1 className="text-2xl font-bold text-gray-800 mt-10 mb-2">Summary</h1>
-          <Table className="w-full border-collapse">
-            <TableHeader>
-              <TableCell className="p-2 border border-gray-300 text-center">队名</TableCell>
-              <TableCell className="p-2 border border-gray-300 text-center">第一节</TableCell>
-              <TableCell className="p-2 border border-gray-300 text-center">第二节</TableCell>
-              <TableCell className="p-2 border border-gray-300 text-center">第三节</TableCell>
-              <TableCell className="p-2 border border-gray-300 text-center">第四节</TableCell>
-              {sections >= 4 ? (
-                <TableCell className="p-2 border border-gray-300 text-center">第一加时</TableCell>
-              ) : (<></>)}
-              {sections >= 5 ? (
-                <TableCell className="p-2 border border-gray-300 text-center">第二加时</TableCell>
-              ) : (<></>)}
-              <TableCell className="p-2 border border-gray-300 text-center font-bold">总分</TableCell>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamAname}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[0]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[1]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[2]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[3]}</TableCell>
-                {sections >= 4 ? (
-                    <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[4]}</TableCell>
-                ) : (<></>)}
-                {sections >= 5 ? (
-                    <TableCell className="p-2 border border-gray-300 text-center">{teamAscores[5]}</TableCell>
-                ) : (<></>)}
-                <TableCell className="p-2 border border-gray-300 text-center font-bold">{teamAscores.reduce((acc, score) => acc + score, 0)}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamBname}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[0]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[1]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[2]}</TableCell>
-                <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[3]}</TableCell>
-                {sections >= 4 ? (
-                    <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[4]}</TableCell>
-                ) : (<></>)}
-                {sections >= 5 ? (
-                    <TableCell className="p-2 border border-gray-300 text-center">{teamBscores[5]}</TableCell>
-                ) : (<></>)}
-                <TableCell className="p-2 border border-gray-300 text-center font-bold">{teamBscores.reduce((acc, score) => acc + score, 0)}</TableCell>
-            </TableRow>
-            </TableBody>
-          </Table>
-        </div>
-    );
+function formatStatsDataForTable(
+  sections: number,
+  teamAname: string,
+  teamBname: string,
+  teamAscores: number[],
+  teamBscores: number[]
+) {
+  const columns = ["队名", "第一节", "第二节", "第三节", "第四节"];
+  const dataTeamA = [teamAname, ...teamAscores.slice(0, 4)];
+  const dataTeamB = [teamBname, ...teamBscores.slice(0, 4)];
+  if (sections >= 4) {
+    columns.push("第一加时");
+    dataTeamA.push(teamAscores[4]);
+    dataTeamB.push(teamBscores[4]);
+  }
+  if (sections >= 5) {
+    columns.push("第二加时");
+    dataTeamA.push(teamAscores[5]);
+    dataTeamB.push(teamBscores[5]);
+  }
+  columns.push("总分");
+  dataTeamA.push(teamAscores.reduce((acc, score) => acc + score, 0));
+  dataTeamB.push(teamBscores.reduce((acc, score) => acc + score, 0));
+
+  return {
+    columns,
+    dataTeamA,
+    dataTeamB,
+  };
 }
 
-export default MatchSumTable
+const MatchSumTable: React.FC<MatchSumTableProps> = ({
+  sections,
+  teamAname,
+  teamBname,
+  teamAscores,
+  teamBscores,
+}) => {
+  const { columns, dataTeamA, dataTeamB } = formatStatsDataForTable(
+    sections,
+    teamAname,
+    teamBname,
+    teamAscores,
+    teamBscores
+  );
+  return (
+    <div className="p-5 bg-gray-50 rounded-lg overflow-x-auto">
+      <h1 className="text-2xl font-bold text-gray-800 mt-10 mb-2">Summary</h1>
+
+      <Table className="w-full border-collapse">
+        <TableHeader>
+          {columns.map((col) => (
+            <TableColumn
+              key={col}
+              className="p-2 border border-gray-300 text-center"
+            >
+              {col}
+            </TableColumn>
+          ))}
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            {dataTeamA.map((cell, idx) => (
+              <TableCell
+                key={`teamA_${idx}`}
+                className="p-2 border border-gray-300 text-center"
+              >
+                {cell}
+              </TableCell>
+            ))}
+          </TableRow>
+          <TableRow>
+            {dataTeamB.map((cell, idx) => (
+              <TableCell
+                key={`teamA_${idx}`}
+                className="p-2 border border-gray-300 text-center"
+              >
+                {cell}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default MatchSumTable;
